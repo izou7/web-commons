df78bd653ddd575fb06eac5a6e9ec0217d454c8d
zhangxiaoyinghao <zhangxiaoyinghao@sina.com>
2014-12-02 10:35:50 +0800

    commit

---
+++ src/main/java/cn/zy/commons/webdev/listener/ConfigureLoaderListener.java
@@ -0,0 +1,58 @@
+package cn.zy.commons.webdev.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import cn.zy.commons.webdev.props.ApplicationConfiguration;
+
+/**
+ * Load properties from '.properties' file when web container starting.
+ * 
+ * @author LuoChuan
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class ConfigureLoaderListener implements ServletContextListener {
+
+	/*
+	 * (non-Javadoc)
+	 * @see javax.servlet.ServletContextListener#contextDestroyed(javax.servlet.
+	 * ServletContextEvent)
+	 */
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * javax.servlet.ServletContextListener#contextInitialized(javax.servlet
+	 * .ServletContextEvent)
+	 */
+	@Override
+	public void contextInitialized(ServletContextEvent event) {
+
+		// Get application configuration location.
+		String appConfigLocation = event.getServletContext().getInitParameter(
+				"appConfigLocation");
+
+		// Check application configuration file.
+		if (appConfigLocation == null) {
+			appConfigLocation = "classpath:config.properties";
+		}
+
+		// Load configuration file.
+		if (appConfigLocation.startsWith("classpath:")) {
+
+			// Load from classpath.
+			ApplicationConfiguration.load(this.getClass().getResourceAsStream(
+					"/" + appConfigLocation.substring(10).trim()));
+		} else {
+
+			// Load from Web root.
+			ApplicationConfiguration.load(event.getServletContext()
+					.getResourceAsStream(appConfigLocation.trim()));
+		}
+	}
+
+}
